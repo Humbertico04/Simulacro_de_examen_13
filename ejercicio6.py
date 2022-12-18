372d126c0651101b3f1548da89914e93b0d52e1b
Humbertico04 <humbertico2004@gmail.com>
2022-12-18 15:56:41 +0100

    ejercicio6

--- ejercicio6.py
+++ ejercicio6.py
@@ -16,8 +16,43 @@
 #  [2, 5, 8],
 #  [1, 4, 7]]
 
-matrix=[[1,2,3],[4,5,6],[7,8,9]]
-print(len(matrix))
+matrix=[[1,2,3,0],
+        [4,5,6,0],
+        [7,8,9,0]]
 
+# Extra:
+# def traspuesta(matrix):
+#     mat=[]
+#     for i in range(len(matrix[0])): #3
+#         mataux=[]
+#         for j in range(len(matrix)): #4
+#             mataux.append(matrix[j][i])
+#         mat.append(mataux)
+#     return mat
 
-    
+def counter_clockwise(matrix):
+    mat=[]
+    for i in reversed(range(len(matrix[0]))): #3
+        mataux=[]
+        for j in range(len(matrix)): #4
+            mataux.append(matrix[j][i])
+        mat.append(mataux)
+    return mat
+
+def clockwise(matrix):
+    mat=[]
+    for i in range(len(matrix[0])): #3
+        mataux=[]
+        for j in range(len(matrix)): #4
+            mataux.append(matrix[::-1][j][i])
+        mat.append(mataux)
+    return mat
+
+def rotate(matrix, direccion):
+    if direccion=="counter-clockwise":
+        return counter_clockwise(matrix)
+    elif direccion=="clockwise":
+        return clockwise(matrix)
+
+print(rotate(matrix,"clockwise"))
+print(rotate(matrix,"counter-clockwise"))
